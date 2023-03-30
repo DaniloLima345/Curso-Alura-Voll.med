f85be10faa28b107ff4b2f722746a53c0547e2c0
DaniloLima345 <86997053+DaniloLima345@users.noreply.github.com>
2023-03-30 01:04:03 -0300

    Criacao de uma nova migration para alterar a tabela medicos
    Criação de um alter table em uma nova migration para adicionar a coluna telefone na tabela medicos.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,12 @@ import med.voll.api.medico.MedicoRepository;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-	
+
 	@Autowired
 	private MedicoRepository repository;
 
 	@PostMapping
+	@Transactional
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
 	}
